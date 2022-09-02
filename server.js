616a262a1aa65630f84a45c4be1be3a7c9b8a233
Riyan <59615906+riyanhossain@users.noreply.github.com>
2022-09-02 20:21:46 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+
+const data = require('./db.js');
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./db');
+const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 

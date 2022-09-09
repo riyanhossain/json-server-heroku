96e243813c61a2f2ca9cf30fffd507474fb95ad7
Riyan <59615906+riyanhossain@users.noreply.github.com>
2022-09-09 21:30:36 +0600

    Create transactions.js

---
+++ transactions.js
@@ -0,0 +1,68 @@
+[
+    {
+      "name": "Pendrive",
+      "amount": "500",
+      "type": "expense",
+      "id": 3
+    },
+    {
+      "name": "upori income",
+      "type": "income",
+      "amount": 20000,
+      "id": 5
+    },
+    {
+      "name": "Fiverr",
+      "type": "income",
+      "amount": 12000,
+      "id": 6
+    },
+    {
+      "name": "upwork",
+      "type": "income",
+      "amount": 10000,
+      "id": 7
+    },
+    {
+      "name": "Bottle",
+      "type": "expense",
+      "amount": 1000,
+      "id": 8
+    },
+    {
+      "name": "Med",
+      "type": "expense",
+      "amount": 15000,
+      "id": 9
+    },
+    {
+      "name": "Drink",
+      "type": "expense",
+      "amount": 120,
+      "id": 10
+    },
+    {
+      "name": "New T-Shirt",
+      "type": "expense",
+      "amount": 1200,
+      "id": 11
+    },
+    {
+      "name": "Sales",
+      "type": "income",
+      "amount": 12000,
+      "id": 12
+    },
+    {
+      "name": "Bonus",
+      "type": "income",
+      "amount": 400,
+      "id": 13
+    },
+    {
+      "name": "Travel",
+      "type": "expense",
+      "amount": 500,
+      "id": 14
+    }
+  ]

734dc61009251d43d2d69b2ef722ad53786ba02d
Riyan <59615906+riyanhossain@users.noreply.github.com>
2022-09-09 21:38:08 +0600

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const todos = require('./todos');
 const videos = require('./videos');
 const tags = require('./tags');
+const transactions = require('./transactions');
 
 module.exports = {
     todos,
     videos,
-    tags
+    tags,
+    transactions
 }
